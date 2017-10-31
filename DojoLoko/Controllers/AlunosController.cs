b2892e0e8501f6392c1e7a4c0298112552ec591c
Felipe Hintz <felipe.hintz@catolicasc.org.br>
2017-10-31 21:49:27 -0200

    asdasdasdasdas

--- DojoLoko/Controllers/AlunosController.cs
+++ DojoLoko/Controllers/AlunosController.cs
@@ -1,4 +1,5 @@
 ﻿using DojoLoko.Models;
+using DojoLoko.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -40,5 +41,78 @@ namespace DojoLoko.Controllers
 			return View(aluno);
 
 		}
+
+		public ActionResult Editar(int id)
+		{
+			var aluno = _context.Aluno.Include(c => c.Faixa).Include(c => c.TipoDeAssociacao).SingleOrDefault(c => c.ID == id);
+
+			if (aluno == null)
+				return HttpNotFound();
+
+			var viewModel = new AlunoEditarViewModel
+			{
+                Aluno = aluno,
+                Faixa = _context.Faixa.ToList(),
+                TipoDeAssociacao = _context.TipodeAssociacao.ToList()
+			};
+
+			return View("Editar", viewModel);
+		}
+
+        public ActionResult Deletar(int id)
+        {
+            var aluno = _context.Aluno.Include(c => c.Faixa).Include(c => c.TipoDeAssociacao).SingleOrDefault(c => c.ID == id);
+
+            if (aluno == null)
+                return HttpNotFound();
+
+            _context.Aluno.Remove(aluno);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost] // só será acessada com POST
+        public ActionResult Salvar(Aluno aluno) // recebemos um cliente
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new AlunoEditarViewModel
+                {
+                    Aluno = aluno,
+                    Faixa = _context.Faixa.ToList(),
+                    TipoDeAssociacao = _context.TipodeAssociacao.ToList()
+                };
+
+                return View("Editar", viewModel);
+            }
+
+            if (aluno.ID == 0)
+                _context.Aluno.Add(aluno);
+            else
+            {
+                var customerInDb = _context.Aluno.Single(c => c.ID == aluno.ID);
+
+                customerInDb.Nome = aluno.Nome;
+                customerInDb.CPF = aluno.CPF;
+                customerInDb.TipodeAssociacaoId = aluno.TipodeAssociacaoId;
+                customerInDb.FaixaId = aluno.FaixaId;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Novo()
+        {
+
+            var viewModel = new AlunoEditarViewModel
+            {
+                Faixa = _context.Faixa.ToList(),
+                TipoDeAssociacao = _context.TipodeAssociacao.ToList()
+            };
+            return View("Editar", viewModel);
+
+        }
 	}
 }
---
+++ DojoLoko/ViewModels/AlunoEditarViewModel.cs
@@ -0,0 +1,33 @@
+﻿using DojoLoko.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DojoLoko.ViewModels
+{
+    public class AlunoEditarViewModel
+    {
+        public Aluno Aluno { get; set;}
+
+        public IEnumerable<Faixa> Faixa { get; set; }
+
+        public IEnumerable<TipoDeAssociacao> TipoDeAssociacao { get; set; }
+
+        public String Titulo
+        {
+            get
+            {
+                if (Aluno != null && Aluno.ID != 0)
+                {
+                    return "Editar cliente";
+                }
+                else
+                {
+                    return "Cadastrar novo cliente";
+                }
+            }
+        }
+
+    }
+}
